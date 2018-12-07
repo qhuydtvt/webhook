73f840fc35655d60dc679c85248fbc952d7bde9c
admin <admin@huynqs-MacBook-Pro-259.local>
2018-12-08 00:37:26 +0700

    .

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 @app.route("/", methods=["GET", "POST"])
 def home():
   print(request.get_json())
-  return 200
+  return "OK"
 
 if __name__ == '__main__':
   app.run(debug=True)
