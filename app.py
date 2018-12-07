404a8ee65e552cd8e54ad336372fcbf01edbf71b
admin <admin@huynqs-MacBook-Pro-259.local>
2018-12-08 01:10:01 +0700

    .

--- app.py
+++ app.py
@@ -4,8 +4,11 @@ app = Flask(__name__)
 
 @app.route("/", methods=["GET", "POST"])
 def home():
-  print(request.get_json())
-  return request.args.get('hub.challenge')
+  if request.method == "GET":
+    return request.args.get('hub.challenge')
+  else:
+    print(request.get_json())
+    return "OK"
 
 if __name__ == '__main__':
   app.run(debug=True)
